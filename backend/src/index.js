e9248f63f9a7c6cda10098017b528c913592498d
vini <viniciuscipolli@gmail.com>
2020-01-17 01:20:19 -0300

    back completo

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
+
 mongoose.connect('mongodb+srv://rocketseat:rocketseat@cluster0-omtey.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 })
 
-mongoose
-app.use(express.json())
 
-app.post('/users', (request, reponse) => {
-    console.log(request.body)
-    return reponse.json({message: "Hello Word!!!"})
-}); 
+app.use(express.json());1
+app.use(routes);
+
+
 
 app.listen(3333);
