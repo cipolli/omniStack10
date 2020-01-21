9232edccdcdf2b2fc5ccf548f150b3c462d4a72d
vini <viniciuscipolli@gmail.com>
2020-01-20 22:04:00 -0300

    se liga
      https://youtu.be/hEdIwDyo6vY?t=3208

---
+++ web/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Header from './Header'
+import './global.css';
+import './App.css';
+
+
+//component: Bloco isolado de HTML, CSS e JS O qual não interfere no restante da aplicação
+//Property: informações que o component Pai possa para o Filho
+//Estate: informações mantidas pelo component (lembrar: imutabilidade)
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitute</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+
+      </main>
+    </div>
+
+  );
+}
+
+export default App;
